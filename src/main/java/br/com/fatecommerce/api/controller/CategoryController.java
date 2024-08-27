d58eacef444a333552f24c54b05ebb3e7d3f9605
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-08-27 20:19:56 -0300

    feat: outros métodos do crud de category

--- src/main/java/br/com/fatecommerce/api/controller/CategoryController.java
+++ src/main/java/br/com/fatecommerce/api/controller/CategoryController.java
@@ -2,14 +2,18 @@ package br.com.fatecommerce.api.controller;
 
 import br.com.fatecommerce.api.entity.Category;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +24,7 @@ import br.com.fatecommerce.api.service.CategoryService;
 @RequestMapping("/api/v1/category")
 @CrossOrigin(value = "*")
 public class CategoryController {
-    
+
     @Autowired
     private CategoryService categoryService;
 
@@ -33,6 +37,24 @@ public class CategoryController {
     @GetMapping("/list")
     public ResponseEntity<Object> listCategory() {
         List<Category> result = categoryService.listCategory();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @DeleteMapping("/delete/{idCategory}")
+    public ResponseEntity<Object> deleteCategory(@PathVariable Long idCategory) {
+        HashMap<String, Object> result = categoryService.deleteCategory(idCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/findCategory/{idCategory}")
+    public ResponseEntity<Object> getCategoryById(@PathVariable Long idCategory){
+        Category result = categoryService.findCategoryById(idCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateCategory(@RequestBody Category category) {
+        Category result = categoryService.updateCategory(category);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
--- src/main/java/br/com/fatecommerce/api/service/CategoryService.java
+++ src/main/java/br/com/fatecommerce/api/service/CategoryService.java
@@ -1,16 +1,20 @@
 package br.com.fatecommerce.api.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fatecommerce.api.entity.Category;
 import br.com.fatecommerce.api.repository.CategoryRepository;
 
 @Service
 public class CategoryService {
-    
+
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -20,6 +24,31 @@ public class CategoryService {
 
     public List<Category> listCategory() {
         return categoryRepository.findAll();
+    }
+
+    public HashMap<String, Object> deleteCategory(Long idCategory) {
+        Optional<Category> category = Optional.ofNullable(categoryRepository.findById(idCategory)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Categoria não encontrada!")));
+
+        categoryRepository.delete(category.get());
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("result", "Categoria: " + category.get().getNameCategory() + " excluída com sucesso!");
+        return result;
+    }
+
+    public Category findCategoryById(Long idCategory) {
+        return categoryRepository.findById(idCategory)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Categoria não encontrada!"));
+    }
+
+    public Category updateCategory(Category category) {
+        if (findCategoryById(category.getId()) != null) {
+            return categoryRepository.saveAndFlush(category);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada!");
+        }
     }
 
 }
